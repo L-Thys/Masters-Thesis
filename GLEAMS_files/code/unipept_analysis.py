84b7820d45b68c64873ec7572d3816af7ca89d56
lotte <lthys54@gmail.com>
2025-03-31 00:51:36 +0200

    unipept analysis

--- GLEAMS_files/code/unipept_analysis.py
+++ GLEAMS_files/code/unipept_analysis.py
@@ -5,11 +5,9 @@ RANKCUTOFF = "phylum"
 CUTOFF = 2
 
 
-# command line arguments: output location (ending in /), list of input files (csv)
-# for i in range(2, len(sys.argv)):
+# command line arguments: output location (ending in /), input file location (ending in /), file name (csv)
+df = pd.read_csv(sys.argv[2]+sys.argv[3])
 
-df = pd.read_csv("results_from_vsc/all_sequences_PSMs_MSV000078777.csv")
- 
 # -- see list of taxa ranks: --
 # for x in df.columns:
 #     if x.endswith("_name"):
@@ -34,6 +32,8 @@ groups = grouped[["peptide"]].agg("count").join(groups)
 groups.rename(columns={"peptide":"count_specific"},inplace=True)
 groups.sort_values(by="count_specific", inplace=True, ascending=False)
 groups.reset_index(inplace=True)
+groups.to_csv(sys.argv[1]+"all_grouped_"+sys.argv[3])
+
 # ----------------------------
 
 # -- select the entries with more peptides than CUTOFF (also drops nan columns) ----
@@ -67,7 +67,8 @@ groups.reset_index(inplace=True)
 # -- select entries with more peptides than CUTOFF and with taxon rank >= RANKCUTOFF (also drops nan columns) ---
 taxon_names = [x[:-5] for x in df.columns if x.endswith("_name")]
 low_ranks = ["no rank"] + taxon_names[:taxon_names.index(RANKCUTOFF)]
-selected = groups[groups["count_specific"]>CUTOFF][~groups["taxon_rank"].isin(low_ranks)]
+selected = groups[groups["count_specific"]>CUTOFF]
+selected = selected[~selected["taxon_rank"].isin(low_ranks)]
 selected.dropna(how='all', axis=1, inplace=True)
 # add count_including_subtaxa and peptides_including_subtaxa
 selected["peptides_including_subtaxa"] = pd.Series()
@@ -79,9 +80,5 @@ for index, row in selected.iterrows():
         peptides = df[df[taxon_rank+"_name"]==taxon_name]["peptide"].tolist()
         selected.at[index, "count_including_subtaxa"] = len(peptides)
         selected.at[index,"peptides_including_subtaxa"] = peptides
-print(selected.drop(columns=["peptides","peptides_including_subtaxa"]))
-selected.to_csv("test.csv")
+selected.to_csv(sys.argv[1]+sys.argv[3])
 # -----------------------------------
-
-# TODO: add peptides of subtaxa as extra columns, and add total count
-#       this does mean we might double count
